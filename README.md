44f07b6dc6f3e86e982b375759c0303319a9f120
Daniel <danielcdc8591@gmail.com>
2021-01-18 09:03:21 +0100

    Añadida librerías y estructura del proyecto

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Trianafy
 
 Proyecto de AD y PSP sobre una API REST.
+
+1. Librerías añadidas: 
+    1) Nodemon
+    2) Babel
+    3) Dotenv
+    4) Express
+    5) Curl
+    6) Body-parser
+    7) Morgan (morgan y morgan-body)
+
+2. Añadida estructura inicial del proyecto (18/01/2021):
+    - controllers
+    - middlewares
+    - models
+    - routes
+    - services
+    - .env
+    - index.js
---
+++ src/index.js
@@ -0,0 +1,26 @@
+// Importación de librerías
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
+import morganBody from "morgan-body";
+
+// Importación de componentes de la API
+import models from './models';
+import controllers from './controllers'
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan('dev'))
+morganBody(app);
+
+
+app.listen(process.env.PORT, () =>
+  console.log(
+    `¡Aplicación de ejemplo escuchando en el puerto ${process.env.PORT}!`
+  )
+);
