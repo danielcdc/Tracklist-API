bdb3d4fd2f88a92328959f985205b5e152382a8b
Daniel <danielcdc8591@gmail.com>
2021-01-28 08:18:40 +0100

    Añadidas las rutas relativas a las canciones dentro de una lista.

--- src/routes/list.js
+++ src/routes/list.js
@@ -9,4 +9,9 @@ router.get('/lists/:id', listController.showListById);
 router.put('/lists/:id', listController.updateListById);
 router.delete('/list/:id', listController.deleteListById);
 
+router.post('/lists/:id1/songs/:id2', listController.addSongToList);
+router.get('/lists/:id1/songs', listController.showAllSongsFromList);
+router.get('/lists/:id1/songs/:id2', listController.showOneSongFromList);
+router.delete('/lists/:id1/songs/:id2', listController.deleteOneSongFromList);
+
 export default router;
